11e11fbb5f729485a91c6babadcdbec3f684ccff
DmitryM <mikhaltchenkov@gmail.com>
2025-10-16 23:12:44 +0500

    initial commit

---
+++ app/src/main/java/dev/mikhalchenkov/isxcatalogviewer/MainActivity.kt
@@ -0,0 +1,47 @@
+package dev.mikhalchenkov.isxcatalogviewer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.mikhalchenkov.isxcatalogviewer.ui.theme.ISXCatalogViewerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ISXCatalogViewerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ISXCatalogViewerTheme {
+        Greeting("Android")
+    }
+}
